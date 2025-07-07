08e6a03ddb9c42ea73f24defa38ff740a8c13e7c
luongdinhkhanhvinh <luongdinhkhanhvinh@gmail.com>
2025-07-07 22:45:16 +0700

    update

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'standalone',
+  // output: 'standalone', // Disabled for Windows build compatibility
   async rewrites() {
     return [
       {
